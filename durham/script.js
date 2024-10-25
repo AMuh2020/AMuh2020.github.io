8aa3d78872ae99f6a34a39439717d6a1f93741eb
AMuh2020 <leasu.muhmeen@gmail.com>
2024-10-25 18:48:29 +0100

    more changes to durham stuff

---
+++ durham/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// console.log("error");
+
+for(let i = 0; i<11;i++) {
+    if(i % 2 === 0) {
+        console.log(i,'is even')
+    } else {
+        console.log(i,'is not even')
+    }
+}
+
